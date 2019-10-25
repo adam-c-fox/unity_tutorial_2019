eeae2877f443f1bde0d8d7b12cc6da395e9eaa27
Jay Lees <jay.lees@me.com>
2019-10-25 10:23:48 +0100

    Add looking and movement scripts

---
+++ unity_tutorial_2019/Assets/Scripts/PlayerLookController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLookController : MonoBehaviour
+{
+    private float xAxisClamp = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ROTATION
+        float mouseSensitivity = 150F;
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xAxisClamp += mouseY;
+        if (xAxisClamp > 90) {
+            xAxisClamp = 90;
+            mouseY = 0;
+            ClampAxisRotationValue(270.0f);
+        } else if (xAxisClamp < -90.0f) {
+            xAxisClamp = -90.0f;
+            mouseY = 0.0f;
+            ClampAxisRotationValue(90.0f);
+        }
+
+        transform.Rotate(Vector3.left * mouseY);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+
+    private void ClampAxisRotationValue(float value) {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
+}
---
+++ unity_tutorial_2019/Assets/Scripts/PlayerMoveController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveController : MonoBehaviour
+{
+    private CharacterController charController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // MOVEMENT
+        float movementSpeed = 6.0F;
+        float hInput = Input.GetAxis("Horizontal") * movementSpeed;
+        float vInput = Input.GetAxis("Vertical") * -movementSpeed;
+
+        Vector3 forwardMovement = transform.forward * hInput;
+        Vector3 rightMovement = transform.right * vInput;
+        Vector3 sum = forwardMovement + rightMovement;
+
+        charController.SimpleMove(sum);
+    }
+}
